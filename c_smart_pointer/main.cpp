e3ea0e36e29e988159474d45cbacd3961a56d9f4
gyubong_park <gyubong_park@tmaxsoft.co.kr>
2024-02-29 16:33:14 +0900

    callabe에 대한 sample 추가

---
+++ c_smart_pointer/main.cpp
@@ -0,0 +1,59 @@
+//
+// Created by bont on 24. 2. 29.
+//
+
+#include <memory>
+#include <iostream>
+
+
+template<class T>
+class CuniquePtr : public std::unique_ptr<T>{
+public:
+    CuniquePtr() : std::unique_ptr<T>(){
+        std::cout << "counstructor" << std::endl;
+    }
+    CuniquePtr(T* object) : std::unique_ptr<T>(){
+        object->print();
+    }
+};
+
+template <class T, void (*deleter)(T*)>
+class CSmartPtr : public std::unique_ptr<T, void (*)(T*)> {
+public:
+    CSmartPtr() : std::unique_ptr<T, void (*)(T*)>(nullptr, deleter) {}
+    CSmartPtr(T* object) : std::unique_ptr<T, void (*)(T*)>(object, deleter) {}
+};
+
+class TEST_A{
+public:
+    TEST_A(std::string ss){
+        ss_ = ss;
+    };
+
+    ~TEST_A(){
+      std::cout << "Destructor activate!!!" << std::endl;
+    };
+
+    void print(){
+        std::cout << "ss : "  << ss_ << std::endl;
+    }
+
+private:
+    std::string ss_;
+};
+
+void deleteTest(TEST_A* a){
+    delete a;
+};
+
+
+int main() {
+    auto aa = new TEST_A("hello");
+    auto ss = CSmartPtr<TEST_A, deleteTest>(aa);
+    //ss->print();
+
+    auto bb = new TEST_A("hi");
+    auto sss = CuniquePtr<TEST_A>(bb);
+
+    return 0;
+}
